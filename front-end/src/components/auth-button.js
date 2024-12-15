f43ff2aa4ba7466add22313bbbf925de3a0f2bcb
chamupathi-mendis <chamupathi.mendis.cs@gmail.com>
2024-12-15 19:43:51 +0000

    add authentication behaviour

---
+++ front-end/src/components/auth-button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const AuthButton = () => {
+
+    const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
+    const handleSignout = () => {
+        logout({ logoutParams: { returnTo: window.location.origin } })
+    }
+    const handleAuth = () => {
+        isAuthenticated ? handleSignout() : loginWithRedirect();
+    }
+
+    const buttonText = isAuthenticated ? "Sign Out" : "Sign In";
+
+    return (
+        <Button
+            color="inherit"
+            onClick={handleAuth}>
+            {buttonText}
+        </Button>
+    );
+}
+
+export default AuthButton;
