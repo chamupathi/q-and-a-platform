f8ab0bdd6a9fdd4b2fbc6c8fbdbcd3dc75a6edef
chamupathi-mendis <chamupathi.mendis.cs@gmail.com>
2024-12-16 03:15:27 +0000

    allow singe field updates

--- backend/controllers/validators/update-question.schema.js
+++ backend/controllers/validators/update-question.schema.js
@@ -2,10 +2,10 @@ const Joi = require('joi');
 
 // Define the validation schema
 const updateQuestionSchema = Joi.object({
-    question: Joi.string().trim().required(),
+    question: Joi.string().trim().optional(),
     answer: Joi.string().trim().optional(),
     updatedBy: Joi.string().email().required(),
-    assignee: Joi.string().email().allow(null).optional(),
+    assignee: Joi.string().email().allow(null, ).optional(),
     properties: Joi.array()
         .items(Joi.string().optional())
         .optional(),
